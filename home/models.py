14583e0bff12bd0e837c86c39cab3153d4988a22
AppSeed Generated Projects <133564452+appseed-projects6@users.noreply.github.com>
2024-06-01 14:39:11 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.CharField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Undefined(models.Model):
+
+    #__Undefined_FIELDS__
+
+    #__Undefined_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Undefined")
+        verbose_name_plural = _("Undefined")
+
+
+
+#__MODELS__END
